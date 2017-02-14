f34321610ad58cc425fba0421f7dd4af7b3725b8
fleenvic <fleenorv@gmail.com>
2017-02-14 08:09:30 -0500

    Add files via upload

--- MySQLAndPHPProject/index.php
+++ MySQLAndPHPProject/index.php
@@ -1,28 +1,24 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
     </head>
     <body>
         <?php
         include("config.php");
-
-        $students_query = "SELECT * FROM tblstudent";
-        $result = $db->query($students_query);
-
+        
+        $query = "SELECT * FROM tblstudent";
+        $result = $db->query($query);
+        
         if ($result == false) {
             $error_message = $db->error;
             echo "<p>An error occurred: $error_message</p>";
             exit();
         }
         ?>
-
-        <table border ="1">
+        
+        <table border="1">
             <thead>
                 <tr>
                     <th>First Name</th>
@@ -34,55 +30,36 @@ and open the template in the editor.
             <tbody>
                 <?php
                 $row_count = $result->num_rows;
-
-                for ($i = 0; $i < $row_count; $i++):
+                
+                for ($i = 0; $i < $row_count; $i++) :
                     $student = $result->fetch_assoc();
-                    ?>
-                    <tr>
-                        <td><?php echo $student['firstName']; ?></td>
-                        <td><?php echo $student['lastName']; ?></td>
-                        <td><?php echo $student['studentID']; ?></td>
-                        <td><a href="mailto:<?php echo $student['eMail']; ?>"><?php echo $student['eMail']; ?></a></td>
-                    </tr>
-
-                    <?php
-                endfor;
                 ?>
-
+                
+                <tr>
+                    <td><?php echo $student['firstName']; ?></td>
+                    <td><?php echo $student['lastName']; ?></td>
+                    <td><?php echo $student['studentID']; ?></td>
+                    <td><a href="mailto:<?php echo $student['eMail']; ?>"><?php echo $student['eMail']; ?></a></td>
+                    <td><form action="delete.php" method="post">
+                        <input type="hidden" name="stuID"
+                               value="<?php echo $student['studentID']; ?>" />
+                        <input type="submit" value="Delete" /></form>
+                    </td>
+                </tr>
+                <?php
+                endfor; 
+                ?>
+                
             </tbody>
         </table>
+        <!-- post link to add new student -->
+            <ul>
+            <p><a href="addStudent.php">Add New Student</a></p>
+                <br></br>
+                <br></br>
+                <br></br>                
 
-        <hr>
-
-        <form action="insert.php" method="post">
-            <table>
-                <thead>
-                    <tr>
-                        <th colspan="2">Add Student</th>                        
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>First Name:</td>
-                        <td><input type="text" name="stuFName" /></td>
-                    </tr>
-                    <tr>
-                        <td>Last Name:</td>
-                        <td><input type="text" name="stuLName" /></td>
-                    </tr>
-                    <tr>
-                        <td>Student ID:</td>
-                        <td><input type="text" name="stuID" /></td>
-                    </tr>
-                    <tr>
-                        <td>Email:</td>
-                        <td><input type="text" name="stuEmail" /></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" align="right"><input type="submit" value ="Add"></td>
-                    </tr>
-                </tbody>
-            </table>
-        </form>
+            </ul>
+        
     </body>
 </html>
